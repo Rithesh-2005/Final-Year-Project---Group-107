f0635e93075afca1e771a0dbe710a758cb1e9150
Rithesh Reddy C <126276027+Rithesh-2005@users.noreply.github.com>
2026-04-10 16:23:41 +0000

    final changes

--- main.py
+++ main.py
@@ -113,4 +113,4 @@ def view_logs():
         return {"status": "error", "message": str(e), "audit_trail": []}
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
